cecf03ecb255a011c39d83f4e0c74f47f403f4f9
Felix <jonfelixrico@gmail.com>
2023-09-29 00:35:54 +0800

    Simplify

--- src/main/java/com/gowpet/pos/config/WebSecurityConfig.java
+++ src/main/java/com/gowpet/pos/config/WebSecurityConfig.java
@@ -2,21 +2,21 @@ package com.gowpet.pos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-import com.gowpet.pos.service.auth.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfiguration {
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
      
     @Bean
@@ -24,18 +24,13 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
      
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-         
-        return authProvider;
+    @SuppressWarnings("deprecation")
+	@Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests().requestMatchers("/login").permitAll()
+        .anyRequest().authenticated();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
- 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
- 
-    //    TODO filterChain
 }
