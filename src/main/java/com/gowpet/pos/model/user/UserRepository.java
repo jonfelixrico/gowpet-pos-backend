e3a4f0bb95faf6142348be155651ff0cb5654d06
Felix <jonfelixrico@gmail.com>
2023-09-28 23:08:50 +0800

    Create additional methods

--- src/main/java/com/gowpet/pos/model/user/UserRepository.java
+++ src/main/java/com/gowpet/pos/model/user/UserRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
-
+	public User findByUsername(String username);
 }
