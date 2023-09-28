e5bf525624f62cd9676cff41315e3a817aeebb79
Felix <jonfelixrico@gmail.com>
2023-09-29 01:16:35 +0800

    Remove encoder

--- src/main/java/com/gowpet/pos/service/UserService.java
+++ src/main/java/com/gowpet/pos/service/UserService.java
@@ -1,6 +1,5 @@
 package com.gowpet.pos.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gowpet.pos.model.user.User;
@@ -9,10 +8,9 @@ import com.gowpet.pos.model.user.UserRepository;
 @Service
 public class UserService {
 	private UserRepository repo;
-	private PasswordEncoder pwEncoder;
 	
 	public User createUser (String username, String password) {
-		return repo.save(new User(username, pwEncoder.encode(password)));
+		return repo.save(new User(username, password));
 	}
 	
 	public User getUserByUsername(String username) {
