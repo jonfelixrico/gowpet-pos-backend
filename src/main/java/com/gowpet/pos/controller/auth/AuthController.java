624075fcbfdf770cf023141818730d0345276638
Felix <jonfelixrico@gmail.com>
2023-09-29 00:38:47 +0800

    Flesh out authenticateUser

--- src/main/java/com/gowpet/pos/controller/auth/AuthController.java
+++ src/main/java/com/gowpet/pos/controller/auth/AuthController.java
@@ -1,5 +1,11 @@
 package com.gowpet.pos.controller.auth;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class AuthController {
+	private AuthenticationManager authManager;
+	
 	@PostMapping
-	public void authenticateUser (@RequestBody LoginRequestDto credentials) {
-		
+	public ResponseEntity<String> authenticateUser (@RequestBody LoginRequestDto credentials) {
+		Authentication authentication = authManager
+                .authenticate(new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<String>("User login successfully!...", HttpStatus.OK);
 	}
 }
