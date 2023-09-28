bbb88283ba7cbb14b49ff38a5fda1b71e8cc3380
Felix <jonfelixrico@gmail.com>
2023-09-29 01:16:43 +0800

    Create createUser method

--- src/main/java/com/gowpet/pos/controller/user/UserController.java
+++ src/main/java/com/gowpet/pos/controller/user/UserController.java
@@ -1,8 +1,23 @@
 package com.gowpet.pos.controller.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
+import com.gowpet.pos.service.UserService;
 
+@RestController("user")
+public class UserController {
+	private PasswordEncoder pwEncoder;
+	private UserService userService;
+
+	/**
+	 * WARNING: This is for debugging only. This should be disabled in production.
+	 * @param dto
+	 */
+	@PostMapping
+	public void createUser (@RequestBody CreateUserDto dto) {
+		userService.createUser(dto.getUsername(), pwEncoder.encode(dto.getPassword()));
+	}
 }
