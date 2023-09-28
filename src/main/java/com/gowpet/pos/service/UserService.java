5b6b50259f92a9854a1e886db1cb544487d5a6b0
Felix <jonfelixrico@gmail.com>
2023-09-28 23:21:33 +0800

    Use passwordEncoder

--- src/main/java/com/gowpet/pos/service/UserService.java
+++ src/main/java/com/gowpet/pos/service/UserService.java
@@ -1,5 +1,6 @@
 package com.gowpet.pos.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gowpet.pos.model.user.User;
@@ -8,9 +9,10 @@ import com.gowpet.pos.model.user.UserRepository;
 @Service
 public class UserService {
 	private UserRepository repo;
+	private PasswordEncoder pwEncoder;
 	
 	public User createUser (String username, String password) {
-		return repo.save(new User(username, password));
+		return repo.save(new User(username, pwEncoder.encode(password)));
 	}
 	
 	public User getUserByUsername(String username) {
