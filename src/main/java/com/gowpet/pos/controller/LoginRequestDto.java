3a23fa84a0cf233c00c1d6e1eaece5a5bbf685fd
Felix <jonfelixrico@gmail.com>
2023-09-28 23:12:11 +0800

    Create login DTO

---
+++ src/main/java/com/gowpet/pos/controller/LoginRequestDto.java
@@ -0,0 +1,21 @@
+package com.gowpet.pos.controller;
+
+public class LoginRequestDto {
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public LoginRequestDto(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+}
