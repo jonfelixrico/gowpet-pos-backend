3eba35edbfc6e4c0393f573b1fd26906ce3d677e
Felix <jonfelixrico@gmail.com>
2023-09-29 00:57:37 +0800

    Add dummy route

--- src/main/java/com/gowpet/pos/controller/auth/AuthController.java
+++ src/main/java/com/gowpet/pos/controller/auth/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +23,10 @@ public class AuthController {
                 .authenticate(new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseEntity<String>("User login successfully!...", HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public void getSession () {
+		//	TODO remove this
 	}
 }
