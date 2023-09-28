2f4774a645de1e7fb36ba80d085c58f9c46eaf89
Felix <jonfelixrico@gmail.com>
2023-09-29 01:14:01 +0800

    Create CreateUserDto

---
+++ src/main/java/com/gowpet/pos/controller/user/CreateUserDto.java
@@ -0,0 +1,13 @@
+package com.gowpet.pos.controller.user;
+
+public class CreateUserDto {
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
