f65e641b67ef2ca11469c8620fb32cd38c3bc38e
Felix <jonfelixrico@gmail.com>
2023-09-28 22:58:53 +0800

    Add column annots

--- src/main/java/com/gowpet/pos/model/user/User.java
+++ src/main/java/com/gowpet/pos/model/user/User.java
@@ -2,6 +2,7 @@ package com.gowpet.pos.model.user;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +13,11 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private UUID id;
+	
+	@Column(nullable = false, unique = true)
 	private String username;
+	
+	@Column(nullable = false)
 	private String password;
 	
 	public User(UUID id, String username, String password) {
