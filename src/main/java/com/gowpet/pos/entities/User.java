0125de779764f2366e14944b11b536aa4675c809
Felix <jonfelixrico@gmail.com>
2023-09-28 22:30:36 +0800

    Add id

--- src/main/java/com/gowpet/pos/entities/User.java
+++ src/main/java/com/gowpet/pos/entities/User.java
@@ -13,12 +13,23 @@ public class User {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private UUID id;
 	private String username;
+	private String password;
 	
+	public User(UUID id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
 	public UUID getId() {
 		return id;
 	}
 
 	public String getUsername() {
 		return username;
+	}
+
+	public String getPassword() {
+		return password;
 	}
 }
