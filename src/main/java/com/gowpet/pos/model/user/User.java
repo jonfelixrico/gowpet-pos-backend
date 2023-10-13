8ee3da799bbc72ca5ab1f2154e5f4319b076c2a2
Felix <jonfelixrico@gmail.com>
2023-10-14 00:07:41 +0800

    Add default constructor

--- src/main/java/com/gowpet/pos/model/user/User.java
+++ src/main/java/com/gowpet/pos/model/user/User.java
@@ -23,6 +23,9 @@ public class User {
 	@Column(nullable = false)
 	private String password;
 	
+	protected User() {
+	}
+
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
