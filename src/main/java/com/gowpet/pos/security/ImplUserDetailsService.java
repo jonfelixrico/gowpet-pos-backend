e55f082eb94b80559bff5f896d7438c6e57b34b2
Felix <jonfelixrico@gmail.com>
2023-10-13 22:01:21 +0800

    Implement UserDetailsService

---
+++ src/main/java/com/gowpet/pos/security/ImplUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.gowpet.pos.security;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gowpet.pos.service.UserService;
+
+@Service
+public class ImplUserDetailsService implements UserDetailsService {
+	private UserService userService;
+
+	ImplUserDetailsService(UserService userService) {
+		this.userService = userService;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = userService.getUserByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("Username %s is not found", username));
+		}
+		
+		return User.builder()
+				.username(user.getUsername())
+				.password(user.getPassword())
+				.authorities(new SimpleGrantedAuthority("USER"))
+				.build();
+	}	
+}
