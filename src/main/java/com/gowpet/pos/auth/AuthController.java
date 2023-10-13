19e6576778ad325472b6a9aa5f4ace0103dcdc05
Felix <jonfelixrico@gmail.com>
2023-10-13 23:58:49 +0800

    Apply minor changes

--- src/main/java/com/gowpet/pos/auth/AuthController.java
+++ src/main/java/com/gowpet/pos/auth/AuthController.java
@@ -1,16 +1,17 @@
 package com.gowpet.pos.auth;
 
+import java.util.List;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 class AuthController {
 	private JwtService jwtService;
 	private AuthenticationManager authManager;
@@ -22,7 +23,11 @@ class AuthController {
 	
 	@PostMapping("/authenticate")
 	String authenticate(@RequestBody AuthenticateDto dto) {
-		Authentication auth = authManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
+		Authentication auth = authManager.authenticate(new UsernamePasswordAuthenticationToken(
+				dto.getUsername(),
+				dto.getPassword(),
+				List.of(new SimpleGrantedAuthority("USER"))));
+
 		if (!auth.isAuthenticated()) {
 			throw new UsernameNotFoundException("Wrong credentials");
 		}
