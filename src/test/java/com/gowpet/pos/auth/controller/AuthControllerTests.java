f3e87867dcc81a7f3fa2b02cf56d2e88b484e907
Felix <jonfelixrico@gmail.com>
2023-10-14 20:57:21 +0800

    Remove unneeded

--- src/test/java/com/gowpet/pos/auth/controller/AuthControllerTests.java
+++ src/test/java/com/gowpet/pos/auth/controller/AuthControllerTests.java
@@ -4,8 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,8 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
@@ -22,7 +18,6 @@ import static org.mockito.BDDMockito.given;
 import com.gowpet.pos.user.service.User;
 import com.gowpet.pos.user.service.UserService;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AuthControllerTests {
