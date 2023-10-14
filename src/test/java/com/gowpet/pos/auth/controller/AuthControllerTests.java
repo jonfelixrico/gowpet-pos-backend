cce83ba848055454d4b9d76fb9a0423a2402a166
Felix <jonfelixrico@gmail.com>
2023-10-14 20:02:01 +0800

    Add some stuff

--- src/test/java/com/gowpet/pos/auth/controller/AuthControllerTests.java
+++ src/test/java/com/gowpet/pos/auth/controller/AuthControllerTests.java
@@ -1,14 +1,21 @@
 package com.gowpet.pos.auth.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+
+import com.gowpet.pos.user.service.User;
 import com.gowpet.pos.user.service.UserService;
 
 @WebMvcTest(controllers = AuthController.class)
@@ -25,6 +32,11 @@ public class AuthControllerTests {
 
 	@Test
 	public void AuthController_Authenticate_ThrowsError() throws Exception {
+		given(userService.getUserByUsername(ArgumentMatchers.any())).willAnswer(User.builder());
+		var request = post("/authenticate")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"username\": \"user\", \"password\": \"password\" }");
+		mockMvc.perform(request);
 	}
 	
 	@Test
