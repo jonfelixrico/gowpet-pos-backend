b71981edf2c3d2d4d4898fc2826ec7a1271854c6
Felix <jonfelixrico@gmail.com>
2023-10-14 20:26:01 +0800

    Add actual tests

--- src/test/java/com/gowpet/pos/auth/controller/AuthControllerTests.java
+++ src/test/java/com/gowpet/pos/auth/controller/AuthControllerTests.java
@@ -4,13 +4,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -37,16 +34,24 @@ public class AuthControllerTests {
 	private PasswordEncoder passwordEncoder;
 
 	@Test
-	void AuthController_Authenticate_ThrowsError() throws Exception {
+	void AuthController_AuthenticateWithWrongCredentials_ThrowsError() throws Exception {
+		given(userService.getUserByUsername(ArgumentMatchers.any())).willAnswer(answer -> null);
+
+		var request = post("/authenticate")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"username\": \"user\", \"password\": \"wrongpassword\" }");
+
+		mockMvc.perform(request).andExpect(status().is4xxClientError());
 	}
 	
 	@Test
-	void AuthController_Authenticate_ReturnsToken() throws Exception {
+	void AuthController_AuthenticateWithCorrectCredentials_ReturnsToken() throws Exception {
 		given(userService.getUserByUsername(ArgumentMatchers.any())).willAnswer(answer -> new User("user1", passwordEncoder.encode("password")));
 
 		var request = post("/authenticate")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{ \"username\": \"user\", \"password\": \"password\" }");
+
 		mockMvc.perform(request).andExpect(status().isOk());
 	}
 }
