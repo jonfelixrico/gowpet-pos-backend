1b245efd628cee7bc07ad26556e08049b180e592
Felix <jonfelixrico@gmail.com>
2023-10-14 20:01:56 +0800

    Add builder annot

--- src/main/java/com/gowpet/pos/user/service/User.java
+++ src/main/java/com/gowpet/pos/user/service/User.java
@@ -8,7 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 
+@Builder
 @Entity
 @Table(name = "app_user") // Can't use "user" since it's a reserved keyword in some DBs (pg)
 public class User { // TODO turn this into an abstract; decouple from DB
