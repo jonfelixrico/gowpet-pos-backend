d5d7d178c937e3de73e35ec120b2eb66b78043d1
Felix <jonfelixrico@gmail.com>
2023-10-16 00:24:55 +0800

    Rename and adjust visibility

--- src/test/java/com/gowpet/pos/auth/controller/AuthControllerIT.java
+++ src/test/java/com/gowpet/pos/auth/controller/AuthControllerIT.java
@@ -20,7 +20,7 @@ import com.gowpet.pos.user.service.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AuthControllerTests {
+class AuthControllerIT {
 	@Autowired
 	private MockMvc mockMvc;
 	
