a1dca611bf153a9c2388d08b89eb3d20bb02a323
Felix <jonfelixrico@gmail.com>
2023-10-17 01:35:03 +0800

    Add stubs for the test

---
+++ src/test/java/com/gowpet/pos/catalog/controller/CatalogControllerIT.java
@@ -0,0 +1,10 @@
+package com.gowpet.pos.catalog.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CatalogControllerIT {
+
+}
