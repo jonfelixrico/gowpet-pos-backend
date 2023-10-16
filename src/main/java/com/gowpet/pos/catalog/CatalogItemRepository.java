5c28892693136b91e22cd226e308fd79c314c7b9
Felix <jonfelixrico@gmail.com>
2023-10-17 00:30:03 +0800

    Remove findAll

--- src/main/java/com/gowpet/pos/catalog/CatalogItemRepository.java
+++ src/main/java/com/gowpet/pos/catalog/CatalogItemRepository.java
@@ -1,6 +1,5 @@
 package com.gowpet.pos.catalog;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -8,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 
 
 interface CatalogItemRepository extends CrudRepository<CatalogItem, UUID> {
-	List<CatalogItem> findAll();
 	Optional<CatalogItem> findById(String id);
 	boolean existsById(String id);
 }
