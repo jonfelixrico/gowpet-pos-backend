b57813cac6f77952dcd33fa2166b838adb1090e1
Felix <jonfelixrico@gmail.com>
2023-10-17 00:34:55 +0800

    Remove paging methods

--- src/main/java/com/gowpet/pos/catalog/CatalogItemService.java
+++ src/main/java/com/gowpet/pos/catalog/CatalogItemService.java
@@ -1,31 +1,26 @@
 package com.gowpet.pos.catalog;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class CatalogItemService {
-	private CatalogItemRepository repo;
+	private CatalogItemRepository crudRepo;
 	
 	public CatalogItemService(CatalogItemRepository repo) {
-		this.repo = repo;
+		this.crudRepo = repo;
 	}
 
 	public CatalogItem create(CatalogItem catalog) {
-		return repo.save(catalog);
+		return crudRepo.save(catalog);
 	}
 	
 	public CatalogItem update(String id, CatalogItem item) {
-		if (repo.existsById(id)) {
+		if (crudRepo.existsById(id)) {
 			return null;
 		}
 		
-		return repo.save(item.withId(UUID.fromString(id)));
-	}
-	
-	public List<CatalogItem> getCatalogItems() {
-		return repo.findAll();
+		return crudRepo.save(item.withId(UUID.fromString(id)));
 	}
 }
