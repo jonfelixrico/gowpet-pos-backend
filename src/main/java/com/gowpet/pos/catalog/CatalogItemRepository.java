44be424dee4d3692012cd2f023efa55d999acd00
Felix <jonfelixrico@gmail.com>
2023-10-16 23:27:10 +0800

    Add methods to the service

--- src/main/java/com/gowpet/pos/catalog/CatalogItemRepository.java
+++ src/main/java/com/gowpet/pos/catalog/CatalogItemRepository.java
@@ -1,9 +1,11 @@
 package com.gowpet.pos.catalog;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
 
 interface CatalogItemRepository extends CrudRepository<CatalogItem, UUID> {
+	List<CatalogItem> findAll();
 }
--- src/main/java/com/gowpet/pos/catalog/CatalogService.java
+++ src/main/java/com/gowpet/pos/catalog/CatalogService.java
@@ -1,7 +1,22 @@
 package com.gowpet.pos.catalog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class CatalogService {
+	private CatalogItemRepository repo;
+	
+	public CatalogService(CatalogItemRepository repo) {
+		this.repo = repo;
+	}
+
+	public CatalogItem createCatalogItem (CatalogItem catalog) {
+		return repo.save(catalog);
+	}
+	
+	public List<CatalogItem> getCatalogItems() {
+		return repo.findAll();
+	}
 }
