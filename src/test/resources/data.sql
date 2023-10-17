772acafe77d05e4a1c7aa9964816db5a43c0fa00
Felix <jonfelixrico@gmail.com>
2023-10-18 01:36:43 +0800

    Adjust

--- src/test/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO app_user (id, username, password)
-VALUES ('d41df560-b4a1-4fae-8dca-1ce95e3833e4', 'user1', '$2a$10$/FxVvhPKTm0FkuXi6h8NxekWKNTiawo4Szd1YgDLy3T0xCEgSTXEK');
-
-INSERT INTO app_user (id, username, password)
-VALUES ('7a40edee-a86d-4847-9907-af12d1c25f1f', 'user2', '$2a$10$/FxVvhPKTm0FkuXi6h8NxekWKNTiawo4Szd1YgDLy3T0xCEgSTXEK');
-
-INSERT INTO catalog_item (id, name, price, type, created_by, create_dt)
-VALUES ('94ad00eb-cba0-411f-9d78-29bc1e565518', 'item-1', 120.75, 'PRODUCT', 'd41df560-b4a1-4fae-8dca-1ce95e3833e4', TIMESTAMP '2004-10-19 10:23:54+02');
