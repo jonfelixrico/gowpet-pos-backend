4121df83872c82ec85d060b4f20c88eb6150d48c
Felix <jonfelixrico@gmail.com>
2023-10-17 21:36:54 +0800

    Remove type

--- src/main/java/com/gowpet/pos/catalog/controller/CreateProductDto.java
+++ src/main/java/com/gowpet/pos/catalog/controller/CreateProductDto.java
@@ -1,12 +1,9 @@
 package com.gowpet.pos.catalog.controller;
 
-import com.gowpet.pos.catalog.ItemType;
-
 import lombok.Getter;
 
 @Getter
 class CreateProductDto {
 	private String name;
 	private Float price;
-	private ItemType type;
 }
