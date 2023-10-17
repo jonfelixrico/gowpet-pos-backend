1c9c2ee40850ce77e4993280887629004d83b21c
Felix <jonfelixrico@gmail.com>
2023-10-18 01:47:22 +0800

    Modify Auth controller

--- src/test/java/com/gowpet/pos/auth/controller/AuthControllerIT.java
+++ src/test/java/com/gowpet/pos/auth/controller/AuthControllerIT.java
@@ -4,54 +4,44 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.BDDMockito.given;
 
-import com.gowpet.pos.user.service.User;
-import com.gowpet.pos.user.service.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthControllerIT {
 	@Autowired
 	private MockMvc mockMvc;
-	
-	@MockBean
-	private UserService userService;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
 
 	@Test
 	void AuthController_AuthenticateWithWrongCredentials_ThrowsError() throws Exception {
-		given(userService.findByUsername(ArgumentMatchers.any())).willAnswer(answer -> null);
-
 		var request = post("/authenticate")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content("{ \"username\": \"user\", \"password\": \"wrongpassword\" }");
+				.content("""
+						{
+							"username": "user1",
+							"password": "wrongpassword"
+						}
+						""");
 
 		mockMvc.perform(request).andExpect(status().is4xxClientError());
 	}
 	
 	@Test
 	void AuthController_AuthenticateWithCorrectCredentials_ReturnsToken() throws Exception {
-		given(userService.findByUsername(ArgumentMatchers.any()))
-			.willAnswer(answer -> User.builder()
-					.username("user1")
-					.password(passwordEncoder.encode("password"))
-					.build());
-
 		var request = post("/authenticate")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content("{ \"username\": \"user\", \"password\": \"password\" }");
+				.content("""
+						{
+							"username": "user1",
+							"password": "password"
+						}
+						""");
 
 		mockMvc.perform(request).andExpect(status().isOk());
 	}
