c8d21aea609f631be009e3bb160a6d2664fdc6bc
Felix <jonfelixrico@gmail.com>
2023-10-24 13:55:32 +0800

    Add exception handler

--- src/main/java/com/gowpet/pos/catalog/controller/CatalogController.java
+++ src/main/java/com/gowpet/pos/catalog/controller/CatalogController.java
@@ -2,11 +2,16 @@ package com.gowpet.pos.catalog.controller;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +64,10 @@ class CatalogController {
 	@DeleteMapping("/product/{id}")
 	void deleteProduct(@PathVariable String id, @AuthenticationPrincipal UserDetails user) {
 		catalogSvc.delete(id, userSvc.findByUsername(user.getUsername()));
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	ResponseEntity<ErrorResponse> handleNoSuchElement() {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 }
