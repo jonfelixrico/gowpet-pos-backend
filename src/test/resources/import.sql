266936b47dc876e77ccf915e1094fa8ec62557f2
Felix <jonfelixrico@gmail.com>
2023-10-24 23:42:02 +0800

    Add new import lines

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,1 +1,3 @@
 INSERT INTO app_user (id, username, password) VALUES ('d41df560-b4a1-4fae-8dca-1ce95e3833e4', 'user1', '$2a$10$/FxVvhPKTm0FkuXi6h8NxekWKNTiawo4Szd1YgDLy3T0xCEgSTXEK');
+
+INSERT_INTO catalog_item (id, name, price, type, create_by_id, create_dt, update_by, update_dt, update_ctr) VALUES ('3e2d537a-3b2a-476d-804b-9ab4c4556cbf', 'dummy catalog item', 120.00, 'PRODUCT', 'd41df560-b4a1-4fae-8dca-1ce95e3833e4', TIMESTAMP '2023-10-24 23:35:53+08:00', 'd41df560-b4a1-4fae-8dca-1ce95e3833e4', TIMESTAMP '2023-10-24 23:35:53+08:00', 0);
