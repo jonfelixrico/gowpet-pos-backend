3fa88e6815ab86f5cb04c89347af683c7058dcc7
Felix <jonfelixrico@gmail.com>
2023-10-24 23:21:30 +0800

    Adjust models

--- src/main/java/com/gowpet/pos/billing/Billing.java
+++ src/main/java/com/gowpet/pos/billing/Billing.java
@@ -1,16 +1,26 @@
 package com.gowpet.pos.billing;
 
 import java.time.Instant;
+import java.util.List;
 
 import com.gowpet.pos.user.service.User;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder(toBuilder = true)
 @Getter
-public class Billing extends BaseBilling {
+public class Billing {
 	protected String id;
+	
+	protected List<? extends BillingItem> items;
+	
+	protected Double amountOverride;
+	
+	protected String notes;
 	
 	protected Instant createDt;
 	protected User createBy;
--- src/main/java/com/gowpet/pos/billing/service/BillingService.java
+++ src/main/java/com/gowpet/pos/billing/service/BillingService.java
@@ -1,14 +1,17 @@
 package com.gowpet.pos.billing.service;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
-import com.gowpet.pos.billing.BaseBilling;
 import com.gowpet.pos.billing.Billing;
 import com.gowpet.pos.user.service.User;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Service
@@ -28,13 +31,19 @@ public class BillingService {
 		return result.get();
 	}
 	
+	@Getter
+	@AllArgsConstructor(access = AccessLevel.PROTECTED)
 	@SuperBuilder(toBuilder = true)
-	public static class NewBilling extends BaseBilling {
+	public static class NewBilling {
+		private List<String> items;
+		private Double amountOverride;
+		private String notes;
 	}
 	
 	public Billing create(NewBilling newBilling, User author) {
 		var toSaveToDb = BillingDb.builder()
-				.items(newBilling.getItems())
+				// TODO Impl item mapping
+//				.items(newBilling.getItems())
 				.amountOverride(newBilling.getAmountOverride())
 				.notes(newBilling.getNotes())
 				.createDt(Instant.now())
