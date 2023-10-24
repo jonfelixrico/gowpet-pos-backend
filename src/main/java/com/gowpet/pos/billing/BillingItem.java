b6a7b9c8697f0b5e006511b83fe9c83971d7e915
Felix <jonfelixrico@gmail.com>
2023-10-24 23:10:11 +0800

    Revert "Create BaseBillingItem"
    This reverts commit 439f04875f8c493d35808b499cf0e0b74863e43c.

--- src/main/java/com/gowpet/pos/billing/BillingItem.java
+++ src/main/java/com/gowpet/pos/billing/BillingItem.java
@@ -1,11 +1,22 @@
 package com.gowpet.pos.billing;
 
+import com.gowpet.pos.catalog.CatalogItem;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder(toBuilder = true)
 @Getter
-public class BillingItem extends BaseBillingItem {
+public class BillingItem {
 	protected String id;
+	
+	protected CatalogItem item;
+	protected Double price;
+	
+	protected Double quantity;
+	
 	protected Billing billing;
 }
