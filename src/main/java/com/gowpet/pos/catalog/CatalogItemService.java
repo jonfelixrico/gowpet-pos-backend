bde48d58208626d4a926d6bd9d44f5302d37004c
Felix <jonfelixrico@gmail.com>
2023-10-24 12:18:56 +0800

    Create delete method

--- src/main/java/com/gowpet/pos/catalog/CatalogItemService.java
+++ src/main/java/com/gowpet/pos/catalog/CatalogItemService.java
@@ -1,9 +1,13 @@
 package com.gowpet.pos.catalog;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
+
+import com.gowpet.pos.user.service.User;
 
 
 @Service
@@ -19,6 +23,16 @@ public class CatalogItemService {
 		var results = new ArrayList<CatalogItem>();
 		saved.forEach(results::add);
 		return results;
+	}
+	
+	public void delete(String id, User user) {
+		var record = repo.findById(id);
+		if (record.isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		
+		var modifiedRecord = record.get().withDeleteBy(user).withDeleteDt(Instant.now());
+		repo.save(modifiedRecord);
 	}
 
 	// TODO make this paginated
