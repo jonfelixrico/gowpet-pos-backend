db27c27efe310b6a08d52415423532bee2f1f08c
Felix <jonfelixrico@gmail.com>
2023-10-24 16:14:34 +0800

    Fix specification

--- src/main/java/com/gowpet/pos/catalog/CatalogItemSpecifications.java
+++ src/main/java/com/gowpet/pos/catalog/CatalogItemSpecifications.java
@@ -4,8 +4,18 @@ import org.springframework.data.jpa.domain.Specification;
 
 class CatalogItemSpecifications {
 	static Specification<CatalogItem> isNotDeleted() {
-		return (root, query, builder) -> {
+		Specification<CatalogItem> statusNotDeleted = (root, query, builder) -> {
 			return builder.notEqual(root.get(CatalogItem_.status), ItemStatus.DELETED);
 		};
+		
+		Specification<CatalogItem> statusIsNull = (root, query, builder) -> {
+			return builder.isNull(root.get(CatalogItem_.status));
+		};
+		
+		/*
+		 * Just checking for statusNotDeleted doesn't seem to work. I have to null-check first via statusIsNull
+		 * for it to work.
+		 */
+		return Specification.where(statusIsNull).or(statusNotDeleted);
 	}
 }
