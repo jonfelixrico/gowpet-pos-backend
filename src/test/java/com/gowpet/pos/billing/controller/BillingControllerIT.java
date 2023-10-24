7b53cc8191068efbd352b61a8d8e99876e06b4da
Felix <jonfelixrico@gmail.com>
2023-10-24 23:03:45 +0800

    Create initial tests

--- src/test/java/com/gowpet/pos/billing/controller/BillingControllerIT.java
+++ src/test/java/com/gowpet/pos/billing/controller/BillingControllerIT.java
@@ -1,5 +1,9 @@
 package com.gowpet.pos.billing.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,4 +16,10 @@ import org.springframework.test.web.servlet.MockMvc;
 class BillingControllerIT {
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Test
+	void BillingController_AccessNonexistentRecord_ThrowError() throws Exception {
+		mockMvc.perform(get("/billing/non-existent"))
+			.andExpect(status().isNotFound());
+	}
 }
