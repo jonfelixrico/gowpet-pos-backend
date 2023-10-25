5ede8e717e913cad887e38a9123edb9fb25d8d8b
Felix <jonfelixrico@gmail.com>
2023-10-25 22:13:52 +0800

    Create DTO

---
+++ src/main/java/com/gowpet/pos/collection/controller/CollectionReqDto.java
@@ -0,0 +1,9 @@
+package com.gowpet.pos.collection.controller;
+
+import com.gowpet.pos.collection.service.CollectionService.CollectionInput;
+
+import lombok.Getter;
+
+@Getter
+public class CollectionReqDto extends CollectionInput {
+}
