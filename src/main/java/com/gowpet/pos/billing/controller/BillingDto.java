3b877f9535d18316d4ddafb79097c04f4c16bf91
Felix <jonfelixrico@gmail.com>
2023-10-25 20:52:39 +0800

    Remove unnecessary annotations

--- src/main/java/com/gowpet/pos/billing/controller/BillingDto.java
+++ src/main/java/com/gowpet/pos/billing/controller/BillingDto.java
@@ -5,7 +5,12 @@ import java.util.List;
 import com.gowpet.pos.billing.service.BillingService.BillingInput;
 import com.gowpet.pos.billing.service.BillingService.BillingItemInput;
 
+import lombok.Getter;
+
+@Getter
 public class BillingDto extends BillingInput {
 	protected List<BillingItemDto> items;
+	
+	@Getter
 	static class BillingItemDto extends BillingItemInput {}
 }
