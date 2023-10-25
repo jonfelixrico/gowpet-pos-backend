fa0c6256f51145523f18ffaae167316439200c3a
Felix <jonfelixrico@gmail.com>
2023-10-25 22:34:35 +0800

    Add the executor

--- src/main/java/com/gowpet/pos/collection/service/CollectionRepository.java
+++ src/main/java/com/gowpet/pos/collection/service/CollectionRepository.java
@@ -1,6 +1,7 @@
 package com.gowpet.pos.collection.service;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-interface CollectionRepository extends CrudRepository<Collection, String> {
+interface CollectionRepository extends CrudRepository<Collection, String>, JpaSpecificationExecutor<Collection> {
 }
