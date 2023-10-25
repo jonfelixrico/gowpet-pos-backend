8e5b7d7ee47c401cdd8aa80aa61879461c07259b
Felix <jonfelixrico@gmail.com>
2023-10-25 19:33:21 +0800

    Create updateCtr and recordStatus

--- src/main/java/com/gowpet/pos/billing/service/Billing.java
+++ src/main/java/com/gowpet/pos/billing/service/Billing.java
@@ -8,6 +8,8 @@ import com.gowpet.pos.user.service.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -52,4 +54,8 @@ public class Billing {
 	@ManyToOne
 	@JoinColumn
 	protected User updateBy;
+	protected Integer updateCtr;
+	
+	@Enumerated(EnumType.STRING)
+	protected RecordStatus recordStatus;
 }
