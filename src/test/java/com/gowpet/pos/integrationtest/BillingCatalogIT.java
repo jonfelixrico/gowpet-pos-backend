1adc799b58276d9a3a2dd5b96da4bee42bd1506a
Felix <jonfelixrico@gmail.com>
2023-10-25 23:01:53 +0800

    Create IT stub

---
+++ src/test/java/com/gowpet/pos/integrationtest/BillingCatalogIT.java
@@ -0,0 +1,16 @@
+package com.gowpet.pos.integrationtest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@WithMockUser(username = "user1")
+class BillingControllerIT {
+	@Autowired
+	private MockMvc mockMvc;
+}
