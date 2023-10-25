9d214ec8108fac74c60c32ccfc1ed32a1fcd9e05
Felix <jonfelixrico@gmail.com>
2023-10-25 22:10:26 +0800

    Create saving mechanism

--- src/main/java/com/gowpet/pos/collection/service/CollectionService.java
+++ src/main/java/com/gowpet/pos/collection/service/CollectionService.java
@@ -1,6 +1,13 @@
 package com.gowpet.pos.collection.service;
 
+import java.time.Instant;
+
 import org.springframework.stereotype.Service;
+
+import com.gowpet.pos.billing.service.Billing;
+import com.gowpet.pos.user.service.User;
+
+import lombok.Getter;
 
 @Service
 public class CollectionService {
@@ -8,5 +15,23 @@ public class CollectionService {
 
 	CollectionService(CollectionRepository repo) {
 		this.repo = repo;
+	}
+	
+	public Collection create(Billing billing, CollectionInput newCollection, User createBy) {
+		var toSave = Collection.builder()
+				.amountPaid(newCollection.amountPaid)
+				.notes(newCollection.notes)
+				.createDt(Instant.now())
+				.createBy(createBy)
+				.billing(billing)
+				.build();
+		
+		return repo.save(toSave);
+	}
+	
+	@Getter
+	public static class CollectionInput {
+		private Double amountPaid;
+		private String notes;
 	}
 }
