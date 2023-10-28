48c1ac1437cf35526e547fbbf5ab4a89ca967e52
Felix <jonfelixrico@gmail.com>
2023-10-28 16:23:37 +0800

    Adjust the key used

--- generate-rsa.sh
+++ generate-rsa.sh
@@ -6,5 +6,4 @@ openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:204
 # Extract the public key from the private key:
 openssl rsa -pubout -in private_key.pem -out public_key.pem
 
-# Convert RSA public key to X509 PEM format:
-openssl rsa -pubin -in public_key.pem -RSAPublicKey_out -out public_key_x509.pem
+# NOTE: This public key is already in SPKI
