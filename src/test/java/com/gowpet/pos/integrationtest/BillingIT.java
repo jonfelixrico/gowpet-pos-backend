2ac1a5277dcb7465ae8818c14273110fe8b55515
Felix <jonfelixrico@gmail.com>
2023-11-07 01:42:08 +0800

    Add comment

--- src/test/java/com/gowpet/pos/integrationtest/BillingIT.java
+++ src/test/java/com/gowpet/pos/integrationtest/BillingIT.java
@@ -50,6 +50,7 @@ class BillingIT {
 		mockMvc.perform(postReq)
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.items[0].catalogItem.id").value("3e2d537a-3b2a-476d-804b-9ab4c4556cbf"))
+			// The price is defined in import.sql. Just look for the insert statement associated with the id.
 			.andExpect(jsonPath("$.items[0].price").value(40.00))
 			.andExpect(jsonPath("$.items[0].quantity").value(3.0))
 			.andExpect(jsonPath("$.notes").value("This is the create test"))
