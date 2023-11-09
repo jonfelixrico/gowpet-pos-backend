e695ed66d27db9087bf6d1ecd980706cdb1f4523
Felix <jonfelixrico@gmail.com>
2023-11-10 01:06:02 +0800

    Remove specifications

--- src/main/java/com/gowpet/pos/billing/service/Billing_.java
+++
@@ -1,9 +0,0 @@
-package com.gowpet.pos.billing.service;
-
-import jakarta.persistence.metamodel.SingularAttribute;
-import jakarta.persistence.metamodel.StaticMetamodel;
-
-@StaticMetamodel(Billing.class)
-class Billing_ {
-	static volatile SingularAttribute<Billing, RecordStatus> recordStatus;
-}
