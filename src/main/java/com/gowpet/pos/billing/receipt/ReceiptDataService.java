fba5678decd2b52a12b128cb4af4b99cb5273a10
Felix <jonfelixrico@gmail.com>
2023-11-23 20:44:47 +0800

    Create setter for receipt data

--- src/main/java/com/gowpet/pos/billing/receipt/ReceiptDataService.java
+++ src/main/java/com/gowpet/pos/billing/receipt/ReceiptDataService.java
@@ -1,5 +1,9 @@
 package com.gowpet.pos.billing.receipt;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +20,39 @@ public class ReceiptDataService {
      */
     public ReceiptData getReceiptData() {
         var result = repo.findById(ReceiptData.DEFAULT_ID);
-        return result.orElseGet(() -> null);
+        return result.orElse(null);
+    }
+
+    public ReceiptData.ReceiptDataBuilder getBuilder() {
+        var result = getReceiptData();
+        if (result == null) {
+            return ReceiptData.builder().id(ReceiptData.DEFAULT_ID);
+        }
+
+        return result.toBuilder();
+    }
+
+    public ReceiptData setReceiptData(ReceiptDataInput input) {
+        var receiptData = getBuilder()
+                .header(input.getHeader())
+                .address(input.getAddress())
+                .contactNo(input.getContactNo())
+                .snsLink(input.getSnsLink())
+                .snsMessage(input.getSnsMessage())
+                .build();
+
+        return repo.save(receiptData);
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PACKAGE)
+    static class ReceiptDataInput {
+        private String header;
+        private String[] address;
+        private String contactNo;
+
+        private String snsLink;
+        private String snsMessage;
     }
 }
