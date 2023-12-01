34601cc48e527a81a7ea3770996e716de6c5d673
Felix <jonfelixrico@gmail.com>
2023-12-02 00:24:34 +0800

    Exclude ErrorMvcAutoConfiguration

--- src/main/java/com/gowpet/pos/PosApplication.java
+++ src/main/java/com/gowpet/pos/PosApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(
+		// This prevents spring boot from redirecting to /error (default). We don't need /error.
+		exclude = {ErrorMvcAutoConfiguration.class})
 public class PosApplication {
 
 	public static void main(String[] args) {
