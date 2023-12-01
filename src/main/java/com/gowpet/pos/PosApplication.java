318f1b2dd9e76eac6bd9e46026caebdec53ac827
Felix <jonfelixrico@gmail.com>
2023-12-02 00:31:03 +0800

    Revert "Remove redirect to /error"
    This reverts commit e1a3350c8ae53e4d7515864537e488ebfc8b98aa.

--- src/main/java/com/gowpet/pos/PosApplication.java
+++ src/main/java/com/gowpet/pos/PosApplication.java
@@ -1,11 +1,9 @@
 package com.gowpet.pos;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class PosApplication {
 
 	public static void main(String[] args) {
