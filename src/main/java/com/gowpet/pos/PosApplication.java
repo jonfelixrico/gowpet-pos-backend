d84c2bd34dd3b88a62055c1a5d14a00ef24d9dd6
Felix <jonfelixrico@gmail.com>
2023-12-02 00:25:24 +0800

    Revert "Exclude ErrorMvcAutoConfiguration"
    This reverts commit 34601cc48e527a81a7ea3770996e716de6c5d673.

--- src/main/java/com/gowpet/pos/PosApplication.java
+++ src/main/java/com/gowpet/pos/PosApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(
-		// This prevents spring boot from redirecting to /error (default). We don't need /error.
-		exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class PosApplication {
 
 	public static void main(String[] args) {
