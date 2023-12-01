e1a3350c8ae53e4d7515864537e488ebfc8b98aa
Felix <jonfelixrico@gmail.com>
2023-12-02 00:23:43 +0800

    Remove redirect to /error

--- src/main/java/com/gowpet/pos/PosApplication.java
+++ src/main/java/com/gowpet/pos/PosApplication.java
@@ -1,9 +1,11 @@
 package com.gowpet.pos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class PosApplication {
 
 	public static void main(String[] args) {
