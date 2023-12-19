60563a251cc407bb747884c5f95b0ae8aa41d72d
Felix <jonfelixrico@gmail.com>
2023-12-20 00:02:08 +0800

    Use intellij's code format

--- src/main/java/com/gowpet/pos/user/service/User.java
+++ src/main/java/com/gowpet/pos/user/service/User.java
@@ -1,16 +1,7 @@
 package com.gowpet.pos.user.service;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,13 +10,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "app_user") // Can't use "user" since it's a reserved keyword in some DBs (pg)
 public class User { // TODO turn this into an abstract; decouple from DB
-	@Id
-	@GeneratedValue(strategy=GenerationType.UUID)
-	private String id;
-	
-	@Column(nullable = false, unique = true)
-	private String username;
-	
-	@Column(nullable = false)
-	private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User createBy;
+
+    private Boolean isRoot() {
+        return getCreateBy() == null;
+    }
 }
