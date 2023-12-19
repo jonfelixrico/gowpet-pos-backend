1903f79018e6a88ca20bd0cd827550984f7ef05a
Felix <jonfelixrico@gmail.com>
2023-12-20 00:23:21 +0800

    Do encoding at the service level

--- src/main/java/com/gowpet/pos/user/service/UserService.java
+++ src/main/java/com/gowpet/pos/user/service/UserService.java
@@ -1,22 +1,26 @@
 package com.gowpet.pos.user.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	private UserRepository repo;
-	
-	UserService(UserRepository repo) {
-		this.repo = repo;
-	}
+    private final UserRepository repo;
+    private final PasswordEncoder pwEncoder;
 
-	public void create(String username, String password) {
-		repo.save(User.builder()
-				.username(username)
-				.password(password).build());
-	}
-	
-	public User findByUsername(String username) {
-		return repo.findByUsername(username);
-	}
+    UserService(UserRepository repo, PasswordEncoder pwEncoder) {
+        this.repo = repo;
+        this.pwEncoder = pwEncoder;
+    }
+
+    public void create(String username, String password) {
+        repo.save(User.builder()
+                .username(username)
+                .password(pwEncoder.encode((password)))
+                .build());
+    }
+
+    public User findByUsername(String username) {
+        return repo.findByUsername(username);
+    }
 }
