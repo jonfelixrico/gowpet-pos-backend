b2f5a864577cabc190e7807d76e2dbb1c9cbcf89
Felix <jonfelixrico@gmail.com>
2023-12-20 00:44:37 +0800

    Allow /user/root

--- src/main/java/com/gowpet/pos/config/SecurityConfig.java
+++ src/main/java/com/gowpet/pos/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                 		.requestMatchers("/authenticate").permitAll()
                 		.requestMatchers("/authenticate/*").permitAll()
-                		.requestMatchers("/debug/**").permitAll()
+                		.requestMatchers("/user/root").permitAll()
                 		.requestMatchers("/openapi").permitAll()
                 		.requestMatchers("/openapi/**").permitAll()
                 		.requestMatchers(HttpMethod.POST, "/user").permitAll() // TODO remove once registration flow is done
