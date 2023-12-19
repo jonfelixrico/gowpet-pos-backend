09043f24e954005fa5cfde7309a064a8038ca029
Felix <jonfelixrico@gmail.com>
2023-12-20 00:40:49 +0800

    Revert "Do encoding at the service level"
    This reverts commit 1903f79018e6a88ca20bd0cd827550984f7ef05a.

--- src/main/java/com/gowpet/pos/user/service/UserService.java
+++ src/main/java/com/gowpet/pos/user/service/UserService.java
@@ -1,26 +1,22 @@
 package com.gowpet.pos.user.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final UserRepository repo;
-    private final PasswordEncoder pwEncoder;
+	private UserRepository repo;
+	
+	UserService(UserRepository repo) {
+		this.repo = repo;
+	}
 
-    UserService(UserRepository repo, PasswordEncoder pwEncoder) {
-        this.repo = repo;
-        this.pwEncoder = pwEncoder;
-    }
-
-    public void create(String username, String password) {
-        repo.save(User.builder()
-                .username(username)
-                .password(pwEncoder.encode((password)))
-                .build());
-    }
-
-    public User findByUsername(String username) {
-        return repo.findByUsername(username);
-    }
+	public void create(String username, String password) {
+		repo.save(User.builder()
+				.username(username)
+				.password(password).build());
+	}
+	
+	public User findByUsername(String username) {
+		return repo.findByUsername(username);
+	}
 }
