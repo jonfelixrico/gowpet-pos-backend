792e8f286255ad1b471340b25808bd11851b69ce
Felix <jonfelixrico@gmail.com>
2023-12-20 21:21:41 +0800

    Just mock hasRootUserBeenSetUp

--- src/test/java/com/gowpet/pos/integrationtest/RootUserSetupIT.java
+++ src/test/java/com/gowpet/pos/integrationtest/RootUserSetupIT.java
@@ -1,30 +1,36 @@
 package com.gowpet.pos.integrationtest;
 
+import com.gowpet.pos.user.service.RootUserSetupService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration
-@ActiveProfiles("empty")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RootUserSetupIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @SpyBean
+    private RootUserSetupService rootUserSvc;
+
     @Order(1)
     @Test
     void RootUserSetUp_CreateRoot_CanLogin() throws Exception {
+        when(rootUserSvc.hasRootUserBeenSetUp()).thenReturn(false);
+
         mockMvc.perform(post("/user/root").contentType(MediaType.APPLICATION_JSON).content("""
                 {
                 	"username": "new-root",
