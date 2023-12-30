a73f0097c41ec9850ec76c9ce47c82b8ffd05aa0
Felix <jonfelixrico@gmail.com>
2023-12-30 21:55:40 +0800

    Add logging

--- src/main/java/com/gowpet/pos/user/controller/UserController.java
+++ src/main/java/com/gowpet/pos/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.gowpet.pos.user.controller;
 
 import com.gowpet.pos.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
     private final UserService userSvc;
     private final PasswordEncoder pwEncoder;
@@ -23,5 +26,6 @@ public class UserController {
     @PostMapping
     void createUser(@RequestBody CreateUserDto newUser, @AuthenticationPrincipal UserDetails user) {
         userSvc.create(newUser.getUsername(), pwEncoder.encode(newUser.getPassword()), userSvc.findByUsername(user.getUsername()));
+        log.info("Created user {}", newUser.getUsername());
     }
 }
