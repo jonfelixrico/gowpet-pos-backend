8469c639fb7375bafdcaa96d1e4c39822799819d
Felix <jonfelixrico@gmail.com>
2023-12-31 13:47:24 +0800

    chore: user: move encoder to the service level (#61)
    * Format the auth filter
    
    * Use optional
    
    * Create separate service for read operations
    
    * Revert "Create separate service for read operations"
    
    This reverts commit 9b81f92a3f621080f69c8c82bc83bdd52dbde2f0.
    
    * Comment in the sole purpose of FindByUsernameService
    
    * Encode at the service level
    
    * Use the FindByUsernameService
    
    * Adjust comment type

--- src/main/java/com/gowpet/pos/billing/controller/BillingController.java
+++ src/main/java/com/gowpet/pos/billing/controller/BillingController.java
@@ -1,15 +1,5 @@
 package com.gowpet.pos.billing.controller;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.*;
-
 import com.gowpet.pos.billing.service.Billing;
 import com.gowpet.pos.billing.service.BillingItem;
 import com.gowpet.pos.billing.service.BillingService;
@@ -17,92 +7,100 @@ import com.gowpet.pos.billing.service.BillingService.BillingInput;
 import com.gowpet.pos.billing.service.BillingService.BillingItemInput;
 import com.gowpet.pos.catalog.CatalogItemService;
 import com.gowpet.pos.user.service.UserService;
-
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/billing")
 @SecurityRequirement(name = "bearerAuth")
 public class BillingController {
-	private final BillingService billingSvc;
-	private final UserService userSvc;
-	private final CatalogItemService itemSvc;
-	
-	BillingController(BillingService billingSvc, UserService userSvc, CatalogItemService itemSvc) {
-		this.billingSvc = billingSvc;
-		this.userSvc = userSvc;
-		this.itemSvc = itemSvc;
-	}
+    private final BillingService billingSvc;
+    private final UserService userSvc;
+    private final CatalogItemService itemSvc;
 
-	private BillingRespDto.BillingItemRespDto convertItemToDto(BillingItem item) {
-		var catalogItem = BillingRespDto.CatalogItem.builder()
-				.name(item.getCatalogItem().getName())
-				.id(item.getCatalogItem().getId())
-				.build();
+    BillingController(BillingService billingSvc, UserService userSvc, CatalogItemService itemSvc) {
+        this.billingSvc = billingSvc;
+        this.userSvc = userSvc;
+        this.itemSvc = itemSvc;
+    }
 
-		return BillingRespDto.BillingItemRespDto.builder()
-				.price(item.getPrice())
-				.quantity(item.getQuantity())
-				.catalogItem(catalogItem)
-				.priceOverride(item.getPriceOverride())
-				.notes(item.getNotes())
-				.build();
-	}
-	
-	private BillingRespDto convertToDto(Billing billing) {
-		var items = billing.getItems().stream()
-				.map(this::convertItemToDto)
-				.toList();
-		
-		return BillingRespDto.builder()
-				.id(billing.getId())
-				.notes(billing.getNotes())
-				.items(items)
-				.serialNo(billing.getSerialNo())
-				.build();
-				
-	}
+    private BillingRespDto.BillingItemRespDto convertItemToDto(BillingItem item) {
+        var catalogItem = BillingRespDto.CatalogItem.builder()
+                .name(item.getCatalogItem().getName())
+                .id(item.getCatalogItem().getId())
+                .build();
 
-	@GetMapping("/{id}")
-	BillingRespDto getBilling(@PathVariable String id) {
-		return convertToDto(billingSvc.get(id));
-	}
-	
-	@GetMapping
-	ResponseEntity<List<BillingRespDto>> listBilling(@RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "30") Integer itemCount) {
-		var page = billingSvc.list(pageNo, itemCount);
-		return ResponseEntity.ok()
-				.header("X-Total-Count", String.valueOf(page.getTotalPages()))
-				.body(page.stream().map(this::convertToDto).toList());
-	}
-	
-	private BillingInput convertFromDto(BillingReqDto dto) {
-		var inputItems = dto.getItems()
-				.stream()
-				.map(item -> BillingItemInput.builder()
-						.catalogId(item.getCatalogId())
-						.quantity(item.getQuantity())
-						.price(itemSvc.findById(item.getCatalogId()).orElseThrow().getPrice())
-						.priceOverride(item.getPriceOverride())
-						.notes(item.getNotes())
-						.build())
-				.toList();
-		
-		return BillingInput.builder()
-				.notes(dto.getNotes())
-				.items(inputItems)
-				.build();
-	}
-	
-	@PostMapping
-	BillingRespDto createBilling(@RequestBody BillingReqDto newBilling,
-			@AuthenticationPrincipal UserDetails user) {
-		var created =  billingSvc.create(convertFromDto(newBilling), userSvc.findByUsername(user.getUsername()));
-		return convertToDto(created);
-	}
-	
-	@ExceptionHandler(NoSuchElementException.class)
-	ResponseEntity<ErrorResponse> handleNoSuchElement() {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-	}
+        return BillingRespDto.BillingItemRespDto.builder()
+                .price(item.getPrice())
+                .quantity(item.getQuantity())
+                .catalogItem(catalogItem)
+                .priceOverride(item.getPriceOverride())
+                .notes(item.getNotes())
+                .build();
+    }
+
+    private BillingRespDto convertToDto(Billing billing) {
+        var items = billing.getItems().stream()
+                .map(this::convertItemToDto)
+                .toList();
+
+        return BillingRespDto.builder()
+                .id(billing.getId())
+                .notes(billing.getNotes())
+                .items(items)
+                .serialNo(billing.getSerialNo())
+                .build();
+
+    }
+
+    @GetMapping("/{id}")
+    BillingRespDto getBilling(@PathVariable String id) {
+        return convertToDto(billingSvc.get(id));
+    }
+
+    @GetMapping
+    ResponseEntity<List<BillingRespDto>> listBilling(@RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "30") Integer itemCount) {
+        var page = billingSvc.list(pageNo, itemCount);
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(page.getTotalPages()))
+                .body(page.stream().map(this::convertToDto).toList());
+    }
+
+    private BillingInput convertFromDto(BillingReqDto dto) {
+        var inputItems = dto.getItems()
+                .stream()
+                .map(item -> BillingItemInput.builder()
+                        .catalogId(item.getCatalogId())
+                        .quantity(item.getQuantity())
+                        .price(itemSvc.findById(item.getCatalogId()).orElseThrow().getPrice())
+                        .priceOverride(item.getPriceOverride())
+                        .notes(item.getNotes())
+                        .build())
+                .toList();
+
+        return BillingInput.builder()
+                .notes(dto.getNotes())
+                .items(inputItems)
+                .build();
+    }
+
+    @PostMapping
+    BillingRespDto createBilling(@RequestBody BillingReqDto newBilling,
+                                 @AuthenticationPrincipal UserDetails user) {
+        var created = billingSvc.create(convertFromDto(newBilling), userSvc.findByUsername(user.getUsername()).orElseThrow());
+        return convertToDto(created);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    ResponseEntity<ErrorResponse> handleNoSuchElement() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
--- src/main/java/com/gowpet/pos/user/service/UserService.java
+++ src/main/java/com/gowpet/pos/user/service/UserService.java
@@ -1,26 +1,31 @@
 package com.gowpet.pos.user.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository repo;
+    private final PasswordEncoder pwEncoder;
 
-    UserService(UserRepository repo) {
+    UserService(UserRepository repo, PasswordEncoder pwEncoder) {
         this.repo = repo;
+        this.pwEncoder = pwEncoder;
     }
 
     public void create(String username, String password, User createBy) {
         var toSave = User.builder()
                 .username(username)
-                .password(password)
+                .password(pwEncoder.encode(password))
                 .createBy(createBy)
                 .build();
 
         repo.save(toSave);
     }
 
-    public User findByUsername(String username) {
+    public Optional<User> findByUsername(String username) {
         return repo.findByUsername(username);
     }
 }
