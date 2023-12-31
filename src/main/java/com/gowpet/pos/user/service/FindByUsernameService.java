959a42da8129147355955a8c65158fb40bcc6b3b
Felix <jonfelixrico@gmail.com>
2023-12-31 13:45:34 +0800

    Adjust comment type

--- src/main/java/com/gowpet/pos/user/service/FindByUsernameService.java
+++ src/main/java/com/gowpet/pos/user/service/FindByUsernameService.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-/**
+/*
  * The sole purpose of this service is to expose the findByUsername method to the authentication-related code.
  * The reason why we're not using UserService in the authentication-related code is because of the password encoder
  * that we're injecting into UserService. Using UserService in the authentication-related code will cause a cyclical
  * dependency.
  */
+
 @Service
 public class FindByUsernameService {
     private final UserRepository repo;
