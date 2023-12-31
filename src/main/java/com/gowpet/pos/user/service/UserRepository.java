8590ddeadd0acd15056f6fc16a4a7cc47d828b27
Felix <jonfelixrico@gmail.com>
2023-12-31 14:38:28 +0800

    Add UUID

--- src/main/java/com/gowpet/pos/user/service/UserRepository.java
+++ src/main/java/com/gowpet/pos/user/service/UserRepository.java
@@ -3,7 +3,8 @@ package com.gowpet.pos.user.service;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-interface UserRepository extends CrudRepository<User, String> {
+interface UserRepository extends CrudRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 }
