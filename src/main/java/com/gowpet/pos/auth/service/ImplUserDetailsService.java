f9fd5b7bb2ebd8434699a9adb116f13e10517019
Felix <jonfelixrico@gmail.com>
2023-12-31 13:32:56 +0800

    Use the FindByUsernameService

--- src/main/java/com/gowpet/pos/auth/service/ImplUserDetailsService.java
+++ src/main/java/com/gowpet/pos/auth/service/ImplUserDetailsService.java
@@ -1,6 +1,6 @@
 package com.gowpet.pos.auth.service;
 
-import com.gowpet.pos.user.service.UserService;
+import com.gowpet.pos.user.service.FindByUsernameService;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,15 +10,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 class ImplUserDetailsService implements UserDetailsService {
-    private final UserService userService;
+    private final FindByUsernameService svc;
 
-    ImplUserDetailsService(UserService userService) {
-        this.userService = userService;
+    ImplUserDetailsService(FindByUsernameService svc) {
+        this.svc = svc;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userService.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s is not found", username)));
+        var user = svc.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s is not found", username)));
 
         return User.builder()
                 .username(user.getUsername())
