8dae2a84475cc968b9c5cf7be8bc546a2d88d4fd
Felix <jonfelixrico@gmail.com>
2023-12-31 14:09:22 +0800

    Add logging

--- src/main/java/com/gowpet/pos/auth/service/SessionService.java
+++ src/main/java/com/gowpet/pos/auth/service/SessionService.java
@@ -2,6 +2,7 @@ package com.gowpet.pos.auth.service;
 
 import com.gowpet.pos.user.service.FindByUsernameService;
 import com.gowpet.pos.user.service.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class SessionService {
     private final FindByUsernameService svc;
 
@@ -22,6 +24,7 @@ public class SessionService {
 
         // User is not authenticated
         if (authentication instanceof AnonymousAuthenticationToken) {
+            log.warn("getSessionUser was used in an unauthenticated context. Please check for a possible misuse of the method.");
             return Optional.empty();
         }
 
