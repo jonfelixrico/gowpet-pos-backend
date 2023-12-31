0cd8b9947d065d151c53ce721b7376250b88c9e2
Felix <jonfelixrico@gmail.com>
2023-12-31 14:07:33 +0800

    Integrate to UserController

--- src/main/java/com/gowpet/pos/user/controller/UserController.java
+++ src/main/java/com/gowpet/pos/user/controller/UserController.java
@@ -1,28 +1,29 @@
 package com.gowpet.pos.user.controller;
 
+import com.gowpet.pos.auth.service.SessionService;
 import com.gowpet.pos.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
+    private final SessionService sessionSvc;
     private final UserService userSvc;
 
-    UserController(UserService userSvc) {
+    UserController(SessionService sessionSvc, UserService userSvc) {
+        this.sessionSvc = sessionSvc;
         this.userSvc = userSvc;
     }
 
     @PostMapping
-    void createUser(@RequestBody CreateUserDto newUser, @AuthenticationPrincipal UserDetails user) {
-        userSvc.create(newUser.getUsername(), newUser.getPassword(), userSvc.findByUsername(user.getUsername()).orElseThrow());
+    void createUser(@RequestBody CreateUserDto newUser) {
+        var sessionUser = sessionSvc.getSessionUser().orElseThrow();
+        userSvc.create(newUser.getUsername(), newUser.getPassword(), sessionUser);
         log.info("Created user {}", newUser.getUsername());
     }
 }
