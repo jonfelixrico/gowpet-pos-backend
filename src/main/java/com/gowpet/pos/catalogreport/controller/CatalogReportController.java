0dee2cfba8ee7bda8abcc81d2fa7cf9a1d4a00ad
Felix <jonfelixrico@gmail.com>
2024-02-08 00:51:38 +0800

    Create controller

---
+++ src/main/java/com/gowpet/pos/catalogreport/controller/CatalogReportController.java
@@ -0,0 +1,11 @@
+package com.gowpet.pos.catalogreport.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog/report")
+@SecurityRequirement(name = "bearerAuth")
+public class CatalogReportController {
+}
