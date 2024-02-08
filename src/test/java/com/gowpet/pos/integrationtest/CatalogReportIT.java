57a73e1b2a8c52101713ce6a11080e7121045284
Felix <jonfelixrico@gmail.com>
2024-02-09 00:27:41 +0800

    Add some stubs

--- src/test/java/com/gowpet/pos/integrationtest/CatalogReportIT.java
+++ src/test/java/com/gowpet/pos/integrationtest/CatalogReportIT.java
@@ -1,11 +1,27 @@
 package com.gowpet.pos.integrationtest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 @WithMockUser(username = "user1")
 public class CatalogReportIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeTestClass
+    void createTestData() {
+        // TODO create test data
+    }
+
+    @Test
+    void Reporting_RetrieveAllTime_ReturnsReport() {
+        // TODO add test
+    }
 }
