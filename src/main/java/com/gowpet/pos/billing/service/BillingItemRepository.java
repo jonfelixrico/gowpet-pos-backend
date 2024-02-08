c7d481c7f79c1080951a2ab51b3f5f5c8afbb32d
Felix <jonfelixrico@gmail.com>
2024-02-09 04:51:29 +0800

    Apply query adjustments

--- src/main/java/com/gowpet/pos/billing/service/BillingItemRepository.java
+++ src/main/java/com/gowpet/pos/billing/service/BillingItemRepository.java
@@ -2,17 +2,16 @@ package com.gowpet.pos.billing.service;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
 import java.util.List;
 
 public interface BillingItemRepository extends CrudRepository<BillingItem, String> {
     @Query("""
-        SELECT new com.gowpet.pos.billing.service.AggregatedBillingItem(bi.catalogItem.id, bi.price, COUNT(1))
+        SELECT new com.gowpet.pos.billing.service.AggregatedBillingItem(bi.catalogItem.id, bi.price, SUM(bi.quantity))
         FROM BillingItem AS bi
-        WHERE
-            bi.billing.createDt BETWEEN ?1 AND ?2
         GROUP BY bi.catalogItem.id, bi.price
     """)
-    List<AggregatedBillingItem> aggregateItems(Instant start, Instant end);
+    List<AggregatedBillingItem> aggregateItems(@Param("start") Instant start, @Param("end") Instant end);
 }
