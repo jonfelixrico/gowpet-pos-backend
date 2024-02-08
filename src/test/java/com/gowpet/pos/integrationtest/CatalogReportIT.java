df814428e597f33ee3083033ad9e056d9fff4b99
Felix <jonfelixrico@gmail.com>
2024-02-09 02:08:28 +0800

    Adjust seed method

--- src/test/java/com/gowpet/pos/integrationtest/CatalogReportIT.java
+++ src/test/java/com/gowpet/pos/integrationtest/CatalogReportIT.java
@@ -1,6 +1,8 @@
 package com.gowpet.pos.integrationtest;
 
 import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -65,8 +67,22 @@ public class CatalogReportIT {
                 .andExpect(status().isOk());
     }
 
-    @BeforeTestClass
+    private boolean hasBeenSetUp = false;
+
+    /**
+     * Didn't go for BeforeAll since we'd have to use a static method for it.
+     * Adjusting the seed code to be static is going to be a PITA so we're going for this
+     * instead to keep the MockMvc approach.
+     *
+     * @throws Exception
+     */
+    @BeforeEach
     void createTestData() throws Exception {
+        // This is to make this BeforeEach run like BeforeAll
+        if (hasBeenSetUp) {
+            return;
+        }
+
         var itemId1 = createCatalogItem("report-item-1", 10.0);
 		var itemId2 = createCatalogItem("report-item-2", 20.0);
 		var itemId3 = createCatalogItem("report-item-3", 30.0);
@@ -74,6 +90,8 @@ public class CatalogReportIT {
 		for (int i = 1; i <= 10; i++) {
 			createBilling(itemId1, itemId2, itemId3, i);
 		}
+
+        hasBeenSetUp = true;
     }
 
     @Test
